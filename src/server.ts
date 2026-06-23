07d0da19c51c9b0d72de546dbbbae118ae1eea15
monirzkhan <odvutkabbo@gmail.com>
2026-06-23 16:57:42 +0600

    model in milti file and connected db successfully

--- src/server.ts
+++ src/server.ts
@@ -6,14 +6,14 @@ import "dotenv/config";
 const port = config.port;
 async function main(){
     try {
-        // await prisma.$connect();
+        await prisma.$connect();
         console.log("Connected to the database successfully.");
         app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
         });
     } catch (error) {
         console.error("Error starting the server:", error);
-        // await prisma.$disconnect();
+        await prisma.$disconnect();
         process.exit(1);
         
     }
