081e4a9b66231a003a41b23d3b065cfde65e3e9a
monirzkhan <odvutkabbo@gmail.com>
2026-06-25 16:30:49 +0600

    jwt access and refresh token implement

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,9 @@
+import config from "../../config";
 import { prisma } from "../../lib/prisma";
+import { jwtUtils } from "../../utilities/jwt";
 import { ILogin } from "./auth.interface";
 import bcrypt from "bcrypt";
+import jwt, { SignOptions } from "jsonwebtoken";
 
 const loginUserFromDB = async (payload: ILogin) => {
 
@@ -14,10 +17,42 @@ const loginUserFromDB = async (payload: ILogin) => {
 
     const isPasswordMatched = await bcrypt.compare(password, user.password);
 
-    if(!isPasswordMatched){
+    if (!isPasswordMatched) {
         throw new Error('Invalid credentials');
+
     }
-    return user;
+
+    const jwtPayload = {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+        role: user.role
+    }
+
+    // const accessToken = jwt.sign(
+    //     jwtPayload, 
+    //     config.jwt_access_secret, 
+    //     {
+    //     expiresIn: config.jwt_access_expire_in
+    //     } as SignOptions
+    // );
+
+    const accessToken = jwtUtils.createToken(
+        jwtPayload,
+        config.jwt_access_secret,
+        config.jwt_access_expire_in as SignOptions
+    )
+
+    const refreshToken = jwtUtils.createToken(
+        jwtPayload,
+        config.jwt_refresh_secret,
+        config.jwt_refresh_expire_in as SignOptions
+    )
+
+
+    return {
+        accessToken, refreshToken
+    };
 
 }
 
